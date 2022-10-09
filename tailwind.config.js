1de731add9c0051d02009c41b57ce6f210fb67a8
G. Ferry <grgsferry@gmail.com>
2022-10-09 20:14:13 +0700

    fix ignored files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {
-      fontFamily: {
-        amiko: ['"Amiko"', "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
